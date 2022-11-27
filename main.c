939e7b422e0b06ebf6d167bcd115dd02baa20aca
Professional-Kate <professionalkateg@gmail.com>
2022-11-27 12:15:24 +0000

    Testing how to create links and add them into the linked list. I think this works how I expect, I hope

--- main.c
+++ main.c
@@ -16,20 +16,34 @@ struct list {
 
 int main() {
     list_t* createdList = create_list();
-    destroy_list(createdList);
 
-    printf("In main(): \n%p\n", createdList);
-    
+    link_t* firstLink = calloc(1, sizeof(link_t));
+    createdList->first = firstLink;
+
+    int data = 10;
+    firstLink->data = &data;
+
+    int* gottenData = createdList->first->data;
+    printf("%i\n", *gottenData);
+
+    link_t* secondLink = calloc(1, sizeof(link_t));
+    firstLink->next = secondLink;
+
+    int moreData = 234;
+    secondLink->data = &moreData;
+
+    int* data2ElectricBoogaloo = createdList->first->next->data;
+    printf("%i\n", *data2ElectricBoogaloo);
+
+    destroy_list(createdList);
     return 0;
 }
 
 list_t* create_list() {
     list_t* newList = calloc(1, sizeof(list_t));
-    printf("In create_list(): \n%p\n", newList);
     return newList;
 }
 
 void destroy_list(list_t* list) {
-    printf("In destroy_list(): \n%p\n", list);
-    free(list); // we are only freeing the stack memory?
+    free(list); 
 }
