f62c09ed1a35a0d284acc681b8c09eb5b0327c5e
Professional-Kate <professionalkateg@gmail.com>
2022-11-27 11:16:32 +0000

    C makes me cry. I added two new function prototypes in linkedlist.h then did the implementation of them in main.c. Currently, we have 2 methods, one for making a new list and one for destroying a created one

---
+++ linkedList.h
@@ -0,0 +1,24 @@
+//
+// Created by profe on 27/11/2022.
+//
+
+#ifndef C_LINKED_LIST_LINKEDLIST_H
+#define C_LINKED_LIST_LINKEDLIST_H
+
+typedef struct list list_t;
+
+// any functions for the list
+
+// create a new list and return a pointer to it
+list_t* create_list();
+
+// destroy a created list using the passed pointer to the list
+void destroy_list(list_t* list);
+
+// iterator 
+// insertion
+// lookup
+// getting
+// count
+
+#endif //C_LINKED_LIST_LINKEDLIST_H
--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "linkedList.h"
 
-typedef struct link link_t; 
-typedef struct list list_t; 
+typedef struct link link_t;
 
 struct link {
     void* data;
@@ -11,11 +11,25 @@ struct link {
 
 struct list {
     link_t* first;
-    size_t count;
 };
 
 
 int main() {
-   
+    list_t* createdList = create_list();
+    destroy_list(createdList);
+
+    printf("In main(): \n%p\n", createdList);
+    
     return 0;
+}
+
+list_t* create_list() {
+    list_t* newList = calloc(1, sizeof(list_t));
+    printf("In create_list(): \n%p\n", newList);
+    return newList;
+}
+
+void destroy_list(list_t* list) {
+    printf("In destroy_list(): \n%p\n", list);
+    free(list); // we are only freeing the stack memory?
 }
