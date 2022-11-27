1758f75082ca99cd6c46825b0a79eab9afd28522
Professional-Kate <professionalkateg@gmail.com>
2022-11-27 09:52:42 +0000

    added a couple of structs with some typedefs for them, one for each link in the LinkedList and one for the actual container of the list

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct link link_t; 
+typedef struct list list_t; 
+
+struct link {
+    void* data;
+    link_t* next;
+};
+
+struct list {
+    link_t* first;
+    size_t count;
+};
+
 
 int main() {
-    printf("Hello, World!\n");
+   
     return 0;
 }
