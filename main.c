904561799f79caa0531fa75a822f04eb15fad59c
Professional-Kate <professionalkateg@gmail.com>
2022-11-29 16:20:32 +0000

    Updated the destroy_list() method so it no more memory leak :). It now clears all the links before the parent structure

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include "linkedList.h"
 
 typedef struct link link_t;
-link_t* create_link(void* data);
+link_t* create_link(void* data, link_t* next);
 
 struct link {
     void* data;
@@ -19,16 +19,19 @@ int main() {
     list_t* createdList = create_list();
 
     int data = 32;
-    createdList->first = create_link(&data);
+    createdList->first = create_link(&data, NULL);
+    
+    data = 34;
+    createdList->first->next = create_link(&data, NULL);
 
     destroy_list(createdList);
     return 0;
 }
 
-link_t* create_link(void* data) {
+link_t* create_link(void* data, link_t* next) {
     link_t* newLink = calloc(1, sizeof(link_t));
     newLink->data = data;
-    newLink->next = NULL; // not needed but it's nice to be explicit
+    newLink->next = next; 
     
     return newLink;
 }
@@ -39,5 +42,15 @@ list_t* create_list() {
 }
 
 void destroy_list(list_t* list) {
-    free(list); 
+    link_t* firstLink = list->first;
+
+    while (1) {
+        if (firstLink == NULL) break;
+        
+        link_t* temp = firstLink;
+        firstLink = firstLink->next;
+        free(temp);
+    }
+    
+    free(list);
 }
